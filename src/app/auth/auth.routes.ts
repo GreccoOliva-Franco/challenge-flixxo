e548f1d2824351d0a1b645283265fbd767de4e2f
Grecco Oliva, Franco <srgrecco93@gmail.com>
2022-12-30 18:16:53 -0300

    feat(auth): add sign in and refresh tokens endpoints

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -7,7 +7,7 @@ import { AuthController } from './auth.controller';
 const router = Router();
 
 router.post('/signup', AuthController.signUp);
-// router.post('/signin', AuthController.signIn);
-// router.post('/refresh', makeValidateBody(AuthRefreshDto), AuthController.refreshToken);
+router.post('/signin', AuthController.signIn);
+router.post('/refresh', AuthController.refreshTokens);
 
 export default router;
