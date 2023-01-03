f4282e214ec6bd15b953a5c083f2e6254eddef63
Grecco Oliva, Franco <srgrecco93@gmail.com>
2023-01-03 06:27:51 -0300

    feat(auth): add jwt bearer auth security

--- src/app/auth/middlewares/auth.middleware.ts
+++ src/app/auth/middlewares/auth.middleware.ts
@@ -1,20 +1,11 @@
-import { Request, Response, NextFunction } from 'express';
-import { Strategy } from 'passport-jwt'
-import { resolve } from 'path';
+import passport from 'passport';
 
-import { AuthForbiddenError } from '../../../common/errors/auth/auth.error';
+import { bearerStrategy } from '../strategies/auth-jwt.strategy';
+
+passport.use('jwt', bearerStrategy);
 
 export class AuthMiddleware {
-	// static authenticate(req: Request, res: Response, next: NextFunction): void {
-	// 	return
-	// }
-
-	// static rbac(req: Request, res: Response, next: NextFunction): void | Response {
-	// 	const targetUserId = req.params.userId!;
-	// 	const { id: requesterUserId } = req.user!.id!;
-
-	// 	if (userId !== id) return res.status(httpCode.FORBIDDEN).json(new AuthForbiddenError());
-
-	// 	next();
-	// }
+	static bearer() {
+		return passport.authenticate('jwt', { session: false });
+	}
 }
