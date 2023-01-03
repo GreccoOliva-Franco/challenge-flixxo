fe1deb970377ad80696ca9482049ea664af371de
Grecco Oliva, Franco <srgrecco93@gmail.com>
2023-01-03 06:25:07 -0300

    style

--- src/app/users/user.routes.ts
+++ src/app/users/user.routes.ts
@@ -1,11 +1,9 @@
 import { Request, Response, Router } from "express";
-import httpCodes from 'http-status-codes';
 
 import userController from "./user.controller";
 
-import { AuthMiddleware } from "../auth/middlewares/auth.middleware";
-
 const router = Router();
+
 router.get('/', userController.find)
 router.delete('/', userController.delete)
 
